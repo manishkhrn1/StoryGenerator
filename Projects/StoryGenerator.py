de05d26629d247ae576475ff6950c4e8b4acf6bf
manishkhrn1 <manishkhrn1@gmail.com>
2023-03-12 05:14:44 -0400

    Added lists and the function variables required

--- Projects/StoryGenerator.py
+++ Projects/StoryGenerator.py
@@ -1,10 +1,17 @@
 """
 Made by: Manish Khurana
 """
-
-from random import randint
+from random import *
 
 storyDecider = randint(0,1)
+nouns = ['king','queen','warrior','demon','wizard']
+verbs=['fight','explore','adventure','sleep','eat','play']
 
 def StoryOne():
-    
+    name = str(input("Please Enter your name: "))
+    colour = str(input("Please Enter your favourite colour: "))
+    hobby = str(input("Please Enter a hobby of yours: "))
+    food = str(input("Please Enter what you like to eat the most: "))
+    adjective = str(input("how would you describe your friend in one word: "))
+
+
