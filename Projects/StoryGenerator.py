8e4873bb9c3148eb72ff37b5e9bf91c91d9d279c
manishkhrn1 <manishkhrn1@gmail.com>
2023-03-12 05:48:52 -0400

    Generated a random story

--- Projects/StoryGenerator.py
+++ Projects/StoryGenerator.py
@@ -1,17 +1,25 @@
 """
 Made by: Manish Khurana
 """
-from random import *
+import random
 
-storyDecider = randint(0,1)
+
 nouns = ['king','queen','warrior','demon','wizard']
-verbs=['fight','explore','adventure','sleep','eat','play']
+verbs=['fought','explored','kicked','kissed','ate','played']
+vehicles=['Tesla','horse','donkey','ferrari','hatchback']
 
 def StoryOne():
     name = str(input("Please Enter your name: "))
-    colour = str(input("Please Enter your favourite colour: "))
     hobby = str(input("Please Enter a hobby of yours: "))
     food = str(input("Please Enter what you like to eat the most: "))
     adjective = str(input("how would you describe your friend in one word: "))
+    vehicle=random.choice(vehicles)
+    noun = random.choice(nouns)
+    verb= random.choice(verbs)
 
+    print(("Once upon a time, there was a {0} named {1}, who owned a {2} of black colour. One day {1} decided to {3} and later on {1} got tired and went" +
+    " to eat some {4} and found a {5} restaraunt for that and the food {1} got was {5} too, So {1} went to the {5} chef who made the {5} {4} and {6} him."+
+    "The Chef {6} {1} back and {1} ran away on {1}'s {2}.").format(noun,name,vehicle,hobby,food,adjective,verb))
 
+   
+StoryOne()
