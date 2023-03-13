6f6238dcb78de8bb516896eeb9f5c543a7029fef
manishkhrn1 <manishkhrn1@gmail.com>
2023-03-13 00:38:35 -0400

    Added a GUI to display

--- Projects/StoryGenerator.py
+++ Projects/StoryGenerator.py
@@ -2,7 +2,7 @@
 Made by: Manish Khurana
 """
 import random
-
+import tkinter as tk
 
 nouns = ['king','queen','warrior','demon','wizard']
 verbs=['fought','explored','kicked','kissed','ate','played']
@@ -17,9 +17,17 @@ def StoryOne():
     noun = random.choice(nouns)
     verb= random.choice(verbs)
 
-    print(("Once upon a time, there was a {0} named {1}, who owned a {2} of black colour. One day {1} decided to {3} and later on {1} got tired and went" +
+    story = ("Once upon a time, there was a {0} named {1}, who owned a {2} of black colour. One day {1} decided to {3} and later on {1} got tired and went" +
     " to eat some {4} and found a {5} restaraunt for that and the food {1} got was {5} too, So {1} went to the {5} chef who made the {5} {4} and {6} him."+
-    "The Chef {6} {1} back and {1} ran away on {1}'s {2}.").format(noun,name,vehicle,hobby,food,adjective,verb))
+    "The Chef {6} {1} back and {1} ran away on {1}'s {2}.").format(noun,name,vehicle,hobby,food,adjective,verb)
+
+    root = tk.Tk()
+    root.title("Your Random Story")
+    root.geometry("600x600")
+
+    story_label= tk.Label(root, text=story ,font=("Helvetica",24) , wraplength=600)
+    story_label.pack()
+
+    root.mainloop()
 
    
-StoryOne()
